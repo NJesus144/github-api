42b482acecc361cc4d6738860f6e6bf375cfd2e7
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-03-07 21:32:23 -0300

    Criado requisicao, getUser e getRepos com axios

--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -1,76 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
 
 import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
-import { Container, Sidebar, Main } from './styles';
-import { getLangsFrom } from '../../services/api';
+import { Loading, Container, Sidebar, Main } from './styles';
+import { getUser, getRepos, getLangsFrom } from '../../services/api';
 
 const RepositoriesPage = () => {
+  const { login } = useParams();
+
+  const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
+  const [loading, setLoading] = useState(true);
 
-  const user = {
-    login: 'NJesus144',
-    name: 'Nalbert de Jesus',
-    avatar_url: 'https://avatars.githubusercontent.com/u/94402903?v=4',
-    followers: 2,
-    following: 1,
-    company: null,
-    blog: 'https://nalbertjesus.onrender.com/',
-    location: null,
-  };
+  useEffect(() => {
+    const loadData = async () => {
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser(login),
+        getRepos(login),
+      ]);
+      setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
 
-  const repositories = [
-    {
-      id: '1',
-      name: 'Repo 1',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'Javascript',
-    },
-    {
-      id: '2',
-      name: 'Repo 2',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'Javascript',
-    },
-    {
-      id: '3',
-      name: 'Repo 3',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'PHP',
-    },
-    {
-      id: '4',
-      name: 'Repo 4',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'Ruby',
-    },
-    {
-      id: '5',
-      name: 'Repo 5',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'Java',
-    },
-    {
-      id: '6',
-      name: 'Repo 6',
-      description: 'Descrição',
-      html_url: 'https://nalbertjesus.onrender.com/',
-      language: 'Ruby',
-    },
-  ];
+      setLanguages(getLangsFrom(repositoriesResponse.data));
 
-  const languages = getLangsFrom(repositories);
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
 
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
   };
+
+  if (loading) {
+    return <Loading>Carregando...</Loading>;
+  }
 
   return (
     <Container>
